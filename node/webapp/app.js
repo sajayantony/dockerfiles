85d6b301aaeb2d0891545ab96c37b38bb4cddd44
Sajay Antony <sajaya@microsoft.com>
2018-11-06 19:52:08 +0600

    Added bhutan flag

--- node/webapp/app.js
+++ node/webapp/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-
-app.get('/', (req, res) => res.send('Hello World!'))
+var path = require('path');
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/bhutan.png')));
 
 var port = process.env.PORT || 8080;
 app.listen(port, () => { 
